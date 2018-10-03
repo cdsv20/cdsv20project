41dc5ebafad650a526110581737b92ab3f450cf8
ALUMNO-E <ALUMNO-E@UCSP.AULAB>
2018-10-03 09:40:14 -0500

    cambios

--- 3_10_18/main.cpp
+++ 3_10_18/main.cpp
@@ -5,14 +5,14 @@ using namespace std;
 int main()
 {
     int x = 10,y = 20,z = 30;
-    int *arr[3];
+    int *arr[3]; //puntero de puntero
     arr[0] = &x;
     arr[1] = &y;
     arr[2] = &z;
     int **ptr = arr;
     cout << *arr << " = " << &x <<  " "<< *ptr << endl;
-    cout << --**ptr << " = " << &x << endl;
-
+    cout << --(**ptr) << " = " << &x << endl;
+    //declaramos otro puntero para recorrer el arreglo ya que el arreglo es siempre constante y no se puede modificar con ++(*arr)
     cout << &y << endl;
     /*for (int i = 0;i < 3;i++)
         cout << *arr[i] << endl;*/
